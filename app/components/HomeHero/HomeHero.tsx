28319e62f1f3c649da1b8e2a0280f0505b4ff54a
omargamal510 <ogamal249@gmail.com>
2025-11-13 18:47:36 +0200

    general adjustements

--- app/components/HomeHero/HomeHero.tsx
+++ app/components/HomeHero/HomeHero.tsx
@@ -6,21 +6,21 @@ function HomeHero() {
   return (
     <div
       style={{
-        backgroundImage: "url('/home/hero.avif')",
+        backgroundImage: "url('/home/hero.jpg')",
         backgroundPosition: "center center",
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
       }}
       className="hero h-svh "
     >
-      <div className="hero-wrapper wrapper-component text-white flex flex-col items-center justify-center gap-10">
+      <div className="hero-wrapper wrapper-component backdrop-filter backdrop-blur-md text-white flex flex-col items-center justify-center gap-10">
         <Image height={300} width={300} alt="insejam logo" src={"/logo.png"} />
 
         <WhiteTap text="All-in-One Collaboration Platform" />
         <h1 className="text-[7vw] sm:leading-20 md:leading-30 font-semibold">
           Empower Your Team to <br /> Build Smarter, Together
         </h1>
-        <p>
+        <p className="mx-5">
           Insejam connects ideas, people, and workflows in one powerful
           collaborative space, <br className="md:block hidden" /> designed to
           help your organization move faster and stay aligned
